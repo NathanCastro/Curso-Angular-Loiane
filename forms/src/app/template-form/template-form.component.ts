f14724919d852c4ddf72b9c05df79e3dfeb6e83b
Nathan Castro <nathanslc18@gmail.com>
2021-08-20 08:53:52 -0300

    chore(template-driven): finish in class 83-improved the css

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -13,7 +15,8 @@ export class TemplateFormComponent implements OnInit {
     email: ''
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -21,14 +24,75 @@ export class TemplateFormComponent implements OnInit {
   onSubmit(form){
     console.log(form)
 
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+    .pipe(map( res => res))
+      .subscribe(dados => console.log(dados));
   }
   validaTouchedValid(campo){
     return !campo.valid && campo.touched;
   }
+  
   aplicaCssErro(campo){
     return {
       'was-validated' : this.validaTouchedValid(campo),
       'has-feedback' : this.validaTouchedValid(campo)
     }
   }
+
+  consultaCEP(cep, form){
+    cep = cep.replace(/\D/g, '');
+    if (cep != "") {
+      var validacep = /^[0-9]{8}$/;
+    
+      this.resetaDadosForm(form);
+      
+      if(validacep.test(cep)) {         
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+        .pipe(map(dados =>  dados))
+          .subscribe(dados => this.populaDadosForm(dados, form));
+      } 
+    }
+  }
+
+  populaDadosForm(dados, form){
+    // form.setValue({
+    //   nome: form.value.nome,
+    //   email:form.value.email,
+    //   endereco: {
+    //     cep: dados.cep ,
+    //     numero: "" ,
+    //     complemento: dados.complemento,
+    //     rua: dados.logradouro,
+    //     bairro: dados.bairro ,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf 
+    //   }
+    // });
+
+    form.form.patchValue({
+      endereco: {
+        cep: dados.cep ,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro ,
+        cidade: dados.localidade,
+        estado: dados.uf 
+      }
+    });
+
+    //console.log(form);
+  }
+
+  resetaDadosForm(form){
+    form.form.patchValue({
+      endereco: {
+        complemento:null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado:null 
+      }
+    });
+  }
 }
+
